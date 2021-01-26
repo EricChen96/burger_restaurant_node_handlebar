ef2d12a3deaca52d035e63257126c823b4448d46
Eric Chen <ericchen201896@gmail.com>
2021-01-26 07:18:28 -0800

    created burgerseController.js and burger.js

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,11 +5,33 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    cat.all((data) => {
-      const hbsObject = {
-        cats: data,
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+    burger.selectAll((data) => {
+        const burgerList = {
+            burgers: data,
+        };
+        console.log(burgerList);
+        res.render("index", burgerList);
     });
-  });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne({ burger_name: req.body.burger_name, devoured: req.body.devoured }, (result) => {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id/devoured", (req, res) => {
+    const condition = { id: req.params.id };
+    const update = { devoured: req.body.value };
+
+    cat.update(update, condition, (result) => {
+        if (result.affectedRows === 0) {
+            // If no rows were affected, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
