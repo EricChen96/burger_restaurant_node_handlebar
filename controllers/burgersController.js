5c205b30f78261c9eba1cf6ef22248f615915713
Eric Chen <ericchen201896@gmail.com>
2021-01-26 17:08:31 -0800

    added in actionlistener for devoured button. Clicking it devours burger now

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -23,9 +23,9 @@ router.post("/api/burgers", (req, res) => {
 
 router.put("/api/burgers/:id/devoured", (req, res) => {
     const condition = { id: req.params.id };
-    const update = { devoured: req.body.value };
+    const update = { devoured: req.body.devoured };
 
-    cat.update(update, condition, (result) => {
+    burger.updateOne(update, condition, (result) => {
         if (result.affectedRows === 0) {
             // If no rows were affected, then the ID must not exist, so 404
             return res.status(404).end();
