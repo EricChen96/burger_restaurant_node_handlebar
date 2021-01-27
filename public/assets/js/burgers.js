200f57cde6e3ce1ff2dcf89a69f2553f45d09ac3
Eric Chen <ericchen201896@gmail.com>
2021-01-26 17:01:37 -0800

    Added in handlebar index page with burger partials for devoured button

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,55 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(() => {
+    $(".change-devoured").on("click", function () {
+        const id = $(this).data("id");
+
+        const newDevouredState = { devoured: true };
+
+        // Send the PUT request.
+        $.ajax(`/api/burgers/${id}/devoured`, {
+            type: "PUT",
+            // Convert object to JSON
+            data: JSON.stringify(newDevouredState),
+            // Tell the server that this request contains JSON
+            contentType: "application/json; charset=UTF-8",
+        }).then(() => {
+            // Reload the page to get the updated list
+            location.reload();
+        });
+    });
+
+    $(".create-form").on("submit", (event) => {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        const newCat = {
+            name: $("#ca").val().trim(),
+            sleepy: $("[name=sleepy]:checked").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/cats", {
+            type: "POST",
+            data: newCat,
+        }).then(() => {
+            // Reload the page to get the updated list
+            location.reload();
+        });
+    });
+
+    $(".delete-cat").on("click", function (event) {
+        const deletedCat = $(this).data("id");
+        console.log(this);
+        $.ajax("/api/cats/" + deletedCat, {
+            type: "DELETE",
+        }).then(() => {
+            console.log("deleted id ", deletedCat);
+            // Reload the page to get the updated list
+            location.reload();
+        });
+    });
+
+    // Add your code to delete a cat when a ".delete-cat" button is clicked.
+    // ... CODE HERE ...
+
+});
